897555b37e4925a7fcf5505ff007a1d7b84ce5b3
Cesa Gutierrez Tineo <cesar@tineo.mobi>
2012-12-28 13:19:44 -0500

    App de la galeria
    La aplicacion para subir las imagenes a la galeria

--- module/Backend/src/Backend/Controller/IndexController.php
+++ module/Backend/src/Backend/Controller/IndexController.php
@@ -20,6 +20,7 @@ use Backend\Entity\Item;
 use Backend\Form\SectionForm;
 use Backend\Form\GalleryForm;
 use Backend\Form\ItemForm;
+
 use Zend\Validator\File\Size;
 
 class IndexController extends AbstractActionController {
@@ -37,6 +38,46 @@ class IndexController extends AbstractActionController {
 	public function zerofill($num, $zerofill = 2) {
 		return str_pad($num, $zerofill, '0', STR_PAD_LEFT);
 	}
+	
+	public function createThumbs( $pathToImages, $pathToThumbs, $thumbWidth )
+	{
+  // open the directory
+  $dir = opendir( $pathToImages );
+
+  // loop through it, looking for any/all JPG files:
+  while (false !== ($fname = readdir( $dir ))) {
+    // parse path for the extension
+    $info = pathinfo($pathToImages . $fname);
+    // continue only if this is a JPEG image
+    if ( strtolower($info['extension']) == 'jpg' )
+    {
+      echo "Creating thumbnail for {$fname} <br />";
+
+      // load image and get image size
+      $img = imagecreatefromjpeg( "{$pathToImages}{$fname}" );
+      $width = imagesx( $img );
+      $height = imagesy( $img );
+
+      // calculate thumbnail size
+      $new_width = $thumbWidth;
+      $new_height = floor( $height * ( $thumbWidth / $width ) );
+
+      // create a new temporary image
+      $tmp_img = imagecreatetruecolor( $new_width, $new_height );
+
+      // copy and resize old image into new image
+      imagecopyresized( $tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+
+      // save thumbnail into a file
+      imagejpeg( $tmp_img, "{$pathToThumbs}{$fname}" );
+     
+    }
+  }
+  // close the directory
+  closedir( $dir );
+}
+	
+	
 	public function indexAction() {
 		$viewModel = new ViewModel();
 		$secciones = $this->getEntityManager()
@@ -93,7 +134,7 @@ class IndexController extends AbstractActionController {
 					'attributes' => array('type' => 'hidden',
 							'value' => $this->getEvent()->getRouteMatch()->getParam('id')),));
 
-			echo $this->getEvent()->getRouteMatch()->getParam('id');
+			
 
 		} elseif ($this->getEvent()->getRouteMatch()->getParam('type')	== "item") {
 			$form = new ItemForm();
@@ -103,7 +144,7 @@ class IndexController extends AbstractActionController {
 							->getRouteMatch()
 							->getParam('id')),));
 
-			echo $this->getEvent()->getRouteMatch()->getParam('id');
+			//echo $this->getEvent()->getRouteMatch()->getParam('id');
 		} else {
 			$form = new SectionForm();
 		}
@@ -116,7 +157,10 @@ class IndexController extends AbstractActionController {
 
 		$request = $this->getRequest();
 		if ($request->isPost()) {
-
+			//echo $form->getName();
+			
+			
+			
 			if ($form->getName() == "Item") {
 				/******/
 				$nonFile = $request->getPost()->toArray();
@@ -142,8 +186,54 @@ class IndexController extends AbstractActionController {
 					} //set formElementErrors
 					$form->setMessages(array('fileupload' => $error));
 				} else {
-					$adapter->setDestination(getcwd() . "/public/_gallery");
+					
+					$g = $this->getEntityManager()
+					->getRepository('Backend\Entity\Gallery')
+					->findOneBy(array('id'=> $this->getEvent()->getRouteMatch()->getParam('id') ));
+					
+					echo getcwd() . $g->getPath(), getcwd() . $g->getPath()."/thumbs"."<br/>";
+					
+									
+					$adapter->setDestination(getcwd() . $g->getPath());
+					chmod(getcwd() . $g->getPath() .$File['name'], 0777);
+					
+					$this->createThumbs(getcwd() . $g->getPath(), getcwd() . $g->getPath()."/thumbs",100);
+					
+					
+					/*if (!copy(getcwd() . $g->getPath()."/".$File['name'], getcwd() . $g->getPath()."/thumbs"."/".$File['name'])) {
+						echo "Error al copiar ".getcwd() . $g->getPath()."/".$File['name']."...\n";
+					}*/
+					
+					
+					$path = substr($g->getPath(), 7 ) . "/".$File['name'];
+					$path_thumb =  substr($g->getPath(), 7 ) . "/thumbs/".$File['name'];
+					
+					$i =  new Item();
+					$i->setDescripcion($request->getPost("description"));
+					$i->setComment($request->getPost("comentario"));
+					$i->setId_gallery($this->getEvent()->getRouteMatch()->getParam('id'));
+					
+					$i->setPath($path);
+					$i->setPath_thumb($path_thumb);
+					
+					$i->setDeleted(0);
+					$i->setVisible(1);
+					
+					
+					$this->getEntityManager()->persist($i);
+					$this->getEntityManager()->flush();
+					
+					
+					echo "<pre>";
+					//echo $this->getEvent()->getRouteMatch()->getParam('type');
+					//echo $this->getEvent()->getRouteMatch()->getParam('id');
 					echo $File['name'];
+					var_dump($request->getPost("description"));
+					var_dump($request->getPost("comentario"));
+					echo "</pre>";
+					
+					
+					
 					if ($adapter->receive($File['name'])) {
 						//	$profile->exchangeArray($form->getData());
 						//	echo 'Profile Name '.$profile->profilename.' upload '.$profile->fileupload;
@@ -151,10 +241,75 @@ class IndexController extends AbstractActionController {
 
 				}
 				/******/
-			}
+			} elseif ($form->getName() == "Gallery"){
+				
+				//echo $File['name'];
+				
+				
+				$gal = $this->getEntityManager()
+				->getRepository('Backend\Entity\Gallery')
+				->findOneBy(array('slug' => $request->getPost("slug"), 
+								  'id_section' => $this->getEvent()->getRouteMatch()->getParam('id') ));
+				
+				//var_dump($gal);
+				
+				if(!empty($gal)){
+					echo "<pre>";
+					print ("Identificador repetido");
+					echo "</pre>";
+					
+				}else{
+				
+				$s = $this->getEntityManager()
+				->getRepository('Backend\Entity\Section')
+				->findOneBy(array('id' => $this->getEvent()->getRouteMatch()->getParam('id')));
+				//echo $s->getUrl();
+								
+				$new_url = $s->getUrl()."/".$request->getPost("slug");
+				$new_path = $s->getPath()."/".$request->getPost("slug");
 
+				
+				
+				//echo getcwd()."$new_path";
+				
+				
+				$g = new Gallery();
+				$g->setDescripcion($request->getPost("description"));
+				$g->setComment($request->getPost("comentario"));
+				$g->setSlug($request->getPost("slug"));	
+				$g->setId_section($this->getEvent()->getRouteMatch()->getParam('id'));
+				
+				$g->setPath($new_path);
+				$g->setUrl($new_url);
+				
+				$g->setDeleted(0);
+				$g->setVisible(1);
+				
+				$this->getEntityManager()->persist($g);
+				$this->getEntityManager()->flush();
+				
+				mkdir( getcwd()."$new_path", 0777,true);
+				mkdir( getcwd()."$new_path"."/thumbs", 0777,true);
+				
+				echo "<pre>";
+				print ('Se creo nueva galeria "'.$request->getPost("description").'" en la seccion "'. $s->getDescripcion().'"');
+				echo "</pre>";
+				
+				/*echo "<pre>";
+				var_dump ($this->getEvent()->getRouteMatch()->getParam('type'));
+				var_dump ($this->getEvent()->getRouteMatch()->getParam('id'));
+				//
+				var_dump($request->getPost("description"));
+				var_dump($request->getPost("slug"));
+				var_dump($request->getPost("comentario"));
+				echo "</pre>";
+				*/
+				}
+				
+			}
+			
 			//good
-			//return $this->redirect()->toRoute('backend');
+			return $this->redirect()->toRoute('backend');
 
 		}
 
