3222e9bdf308a55f5b4faf1661557049519c2c2e
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2013-06-22 11:24:22 -0500

    Modificaciones del sabado 22 de junio

---
+++ module/Application/src/Application/Controller/StandsxController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\View;
+use Zend\View\Model\ViewModel;
+
+
+class StandsxController extends AbstractActionController {
+  public function indexAction() {
+    return new ViewModel ();
+  }
+        
+  public function proyectosAction() {
+    $viewModel = new ViewModel ();
+    
+    $vars = array();
+    $vars['gallery'] = $this->getEvent ()->getRouteMatch ()->getParam ( 'gallery' );
+    
+    // Galería por defecto
+    if($vars['gallery'] === NULL) {
+      $vars['gallery'] = 'extemin2011';
+    }
+    
+    return $viewModel;
+  }
+}
+
---
+++ public/stands/st_gallery.php
@@ -0,0 +1,33 @@
+<?php
+$dir = dirname ( __FILE__ );
+// echo $dir."/_images/portfolio/".$_REQUEST['trend'];
+$imagenes = scandir ( "/home/ftpapoyoweb/apoyopublicitario.com/public/stands/_images/portfolio/" . $_REQUEST ['trend'] );
+?>
+
+<div class="span11" >
+
+	<div class="camera_wrap camera_magenta_skin "
+		id="camera_<?=$_REQUEST['trend']?>">
+<?php
+foreach ( $imagenes as $imagen ) {
+	if ($imagen != "thumb.jpg" && $imagen != "thumbs" && $imagen != "." && $imagen != "..") {
+		?>
+	<div
+			data-thumb="_images/portfolio/<?=$_REQUEST['trend']."/thumbs/".$imagen;?>"
+			data-src="_images/portfolio/<?=$_REQUEST['trend']."/".$imagen;?>">
+			<?php 
+				$imagen = substr($imagen, 0, -4);
+				$name = explode("@",$imagen);
+			?>
+			<div class="camera_caption fadeFromBottom"><?=$name[1]?>, <?=$name[2]?></div>
+		</div>
+	
+	<?php
+	}
+	// echo $imagen."<br/>";
+}
+?>
+	</div>
+	<!-- #camera_wrap_2 -->
+</div>
+
